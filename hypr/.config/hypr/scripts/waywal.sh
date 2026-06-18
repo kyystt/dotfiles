50d7f03887b4ba330958905532df128bba6578ba
kyystt <gui.sobreira10@gmail.com>
2026-06-18 00:43:22 -0300

    more ricing...

--- hypr/.config/hypr/scripts/waywal.sh
+++ hypr/.config/hypr/scripts/waywal.sh
@@ -14,13 +14,15 @@ FULL_PATH="$WALLPAPER_DIR/$choice"
 
 if ! pgrep -x "awww-daemon" > /dev/null; then
     awww-daemon &
-    sleep -.5
+    sleep 0.5
 fi
 
 awww img "$FULL_PATH" --transition-type grow --transition-pos "$(hyprctl cursorpos)" --transition-duration 2
 
 wal -i "$FULL_PATH"
 
-~/.config/waybar/scripts/launch.sh
+~/.config/hypr/scripts/launch.sh
+
+ln -sf "$FULL_PATH" "$HOME/.current_wallpaper"
 
 notify-send "Theme Updated" "Wallpaper: $choice" -i "$FULL_PATH"
